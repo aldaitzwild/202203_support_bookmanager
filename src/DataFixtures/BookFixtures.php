0d08fa2f17b179587a670313b765f9fcbef60f7a
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-05-31 17:32:55 +0200

    Add chapters to my books

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -8,43 +8,44 @@ use Doctrine\Persistence\ObjectManager;
 
 class BookFixtures extends Fixture
 {
+    public const BOOKS = [
+        [  
+            'title' => 'Toto à la plage', 
+            'description' => 'C\'est toto qui va à la plage !', 
+            'nbPages' => 12,
+            'author' => 1,
+        ],
+        [  
+            'title' => 'Dune', 
+            'description' => 'Une vague histoire d\'épicerie.', 
+            'nbPages' => 698,
+            'author' => 2,
+        ],
+        [  
+            'title' => 'L\'empereur-dieu de dune', 
+            'description' => 'Toujours l\'épicerie, mais le tenancier a pris du grade', 
+            'nbPages' => 322,
+            'author' => 2,
+        ],
+        [  
+            'title' => 'Le comte de Monte-Cristo', 
+            'description' => 'De prisonnier à mieux qu\'un youtubeur.', 
+            'nbPages' => 850,
+            'author' => 4,
+        ],
+        [  
+            'title' => 'Les misérables', 
+            'description' => 'Une bande de looser.', 
+            'nbPages' => 728,
+            'author' => 5,
+        ],
+    ];
+
     public function load(ObjectManager $manager): void
     {
 
-        $books = [
-            [  
-                'title' => 'Toto à la plage', 
-                'description' => 'C\'est toto qui va à la plage !', 
-                'nbPages' => 12,
-                'author' => 1,
-            ],
-            [  
-                'title' => 'Dune', 
-                'description' => 'Une vague histoire d\'épicerie.', 
-                'nbPages' => 698,
-                'author' => 2,
-            ],
-            [  
-                'title' => 'L\'empereur-dieu de dune', 
-                'description' => 'Toujours l\'épicerie, mais le tenancier a pris du grade', 
-                'nbPages' => 322,
-                'author' => 2,
-            ],
-            [  
-                'title' => 'Le comte de Monte-Cristo', 
-                'description' => 'De prisonnier à mieux qu\'un youtubeur.', 
-                'nbPages' => 850,
-                'author' => 4,
-            ],
-            [  
-                'title' => 'Les misérables', 
-                'description' => 'Une bande de looser.', 
-                'nbPages' => 728,
-                'author' => 5,
-            ],
-        ];
-
-        foreach($books as $dataBook) {
+        $i = 1;
+        foreach(self::BOOKS as $dataBook) {
             $book = new Book();
             $book
                 ->setTitle($dataBook['title'])
@@ -54,6 +55,8 @@ class BookFixtures extends Fixture
             ;
 
             $manager->persist($book);
+            $this->addReference('book_' . $i, $book);
+            $i++;
         }
 
         $manager->flush();
